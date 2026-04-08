061c19bdf760ceb52fe44214444d43e0fea483df
David Andersson <lejonmanen@gmail.com>
2026-04-08 12:00:47 +0200

    Validate after user submitted

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -5,19 +5,45 @@ const Form = () => {
 	const [formData, setFormData] = useState({
 		name: ''
 	})
+	const [touched, setTouched] = useState(false)  // gäller för hela formuläret
 
+	// Validering av första fältet
 	const nameError = validateName(formData.name)
 	let nameCss = ''
 	let nameErrorCss = 'error'
-	if( nameError ) {
-		nameCss = 'invalid'
-		nameErrorCss = 'error show'
-	} else {
-		nameCss = 'valid'
+	if( touched ) {
+		if( nameError ) {
+			nameCss = 'invalid'
+			nameErrorCss = 'error show'
+		} else {
+			nameCss = 'valid'
+		}
+	}
+	const nameIsValid = nameError === ''
+
+	// TODO: validering av e-post
+
+
+	const fieldsAreValid = nameIsValid // && emailIsValid  // TODO: lägg till kod för e-post här
+	const formIsValid = !touched || fieldsAreValid
+
+	const handleSubmit = event => {
+		// Indikera att användaren har fått en chans att fylla i formuläret - nu är det fritt fram att visa felmeddelanden
+		setTouched(true)
+
+		// Om något har gått fel, avbryt
+		if( !fieldsAreValid ) {
+			// Stoppa standardhändelsen - submit
+			event.preventDefault()
+			return
+		}
+
+		// Göra saker med formulärdatan - till exempel spara eller skicka till ett API
 	}
 
 	return (
 		<div className="form">
+			<form>
 
 			<section className="form-field">
 
@@ -35,9 +61,19 @@ const Form = () => {
 
 			</section>
 
+			<section className="form-field">
+				<button
+					type="submit"
+					disabled={!formIsValid}
+					onClick={handleSubmit}
+					> Spara </button>
+			</section>
+
 			{/* Detta är som en console.log, ha inte kvar i slutliga versionen av ditt formulär... */}
 			<h2> Test av formuläret </h2>
 			<p> {formData.name} </p>
+
+			</form>
 		</div>
 	)
 }
